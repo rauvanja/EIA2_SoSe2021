11bc17bf0e37c91e388a0ad5e0ee6092beb1503e
rauvanja <Vanja@Vanjas-MacBook-Pro.local>
2021-03-22 16:37:19 +0100

    L01 hochladen

---
+++ L01_Recap&Foundation/Übung/Boxes.js
@@ -0,0 +1,7 @@
+"use strict";
+let v = 1;
+v = v + 1;
+console.log(v);
+let a = [7, true, "Hallo"];
+let s = { "zahl": 7, "wahr": true, text: "Hallo" };
+//# sourceMappingURL=Boxes.js.map
---
+++ L01_Recap&Foundation/Übung/Boxes.ts
@@ -0,0 +1,7 @@
+let v:  number = 1;
+v = v + 1;
+console.log(v);
+
+let a = [7, true, "Hallo"];
+
+let s = {"zahl": 7, "wahr": true, text: "Hallo"};
