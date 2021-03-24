2819140e555f51aae6bef64610dedd7cf1542a44
rauvanja <Vanja@Vanjas-MBP.fritz.box>
2021-03-24 19:42:02 +0100

    Ordner L01 erstellt

--- L01_Recap&Foundation/Übung/Boxes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let v = 1;
-v = v + 1;
-console.log(v);
-let a = [7, true, "Hallo"];
-let s = { "zahl": 7, "wahr": true, text: "Hallo" };
-//# sourceMappingURL=Boxes.js.map
--- L01_Recap&Foundation/Übung/Boxes.ts
+++
@@ -1,7 +0,0 @@
-let v:  number = 1;
-v = v + 1;
-console.log(v);
-
-let a = [7, true, "Hallo"];
-
-let s = {"zahl": 7, "wahr": true, text: "Hallo"};
