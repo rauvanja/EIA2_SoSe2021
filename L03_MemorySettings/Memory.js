ae24c29eb1bac2aedee59ddea69f75c0934362d3
rauvanja <Vanja@Vanjas-MBP.fritz.box>
2021-05-01 15:55:43 +0200

    upload L03 2te

--- L03_MemorySettings/Memory.js
+++ L03_MemorySettings/Memory.js
@@ -1,2 +1,24 @@
 "use strict";
+var L03_MemorySettings;
+(function (L03_MemorySettings) {
+    window.addEventListener("load", init);
+    function init(_event) {
+        console.log("Start");
+        let fieldsets = document.querySelectorAll("fieldset");
+        // Install listeners on fieldsets
+        for (let i = 0; i < fieldsets.length; i++) {
+            let fieldset = fieldsets[i];
+            fieldset.addEventListener("change", handleChange);
+            fieldset.addEventListener("input", handleChange);
+        }
+    }
+    function handleChange(_event) {
+        let target = _event.target;
+        console.log();
+        if (_event.type == "change")
+            console.warn("Change: " + target.name + " = " + target.value, _event);
+        else
+            console.log("Input: " + target.name + " = " + target.value, _event);
+    }
+})(L03_MemorySettings || (L03_MemorySettings = {})); //End namespace
 //# sourceMappingURL=Memory.js.map
--- L03_MemorySettings/Memory.ts
+++ L03_MemorySettings/Memory.ts
@@ -1,6 +1,25 @@
-namespace Memory {
+namespace L03_MemorySettings {
 
+    window.addEventListener("load", init);
 
+    function init(_event: Event): void {
+        console.log("Start");
+        let fieldsets: NodeListOf<HTMLFieldSetElement> = document.querySelectorAll("fieldset");
 
+        // Install listeners on fieldsets
+        for (let i: number = 0; i < fieldsets.length; i++) {
+            let fieldset: HTMLFieldSetElement = fieldsets[i];
+            fieldset.addEventListener("change", handleChange);
+            fieldset.addEventListener("input", handleChange);
+        }
+    }
+    function handleChange(_event: Event): void {
+        let target: HTMLInputElement = <HTMLInputElement>_event.target;
+        console.log();
+        if (_event.type == "change")
+            console.warn("Change: " + target.name + " = " + target.value, _event);
+        else
+            console.log("Input: " + target.name + " = " + target.value, _event);
+    }
 
 } //End namespace
